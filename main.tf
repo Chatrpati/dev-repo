03ef26127eaf5f09c4ceca91bcd47c301435cb8e
Shivraj Telang <stelang@blackmountainsystems.local>
2024-05-29 16:22:57 +0530

    added region alises

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
 provider "aws" {
   region = "ap-south-1"
+  alias = "south"
   
 }
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "east"
+  # ... other configuration ...
+}
+
+provider "aws" {
+  region = "us-west-1"
+  alias  = "west"
+  # ... other configuration ...
+}
+
 
 resource "aws_instance" "terraformserver" {
     ami = var.myami
